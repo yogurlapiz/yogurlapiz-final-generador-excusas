fe9a619d69cf51fc86470f3e6a4fdc02aafb9bdb
Vbarbosa <131600563+yogurlapiz@users.noreply.github.com>
2025-01-31 22:35:12 +0000

    First

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let getWho = ["The dog", "My grandma", "The mailman", "My bird"];
+  let getAction = ["ate", "peed", "crushed", "broke"];
+  let getWhat = ["my homework", "my phone", "the car", "my book"];
+  let getWhen = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function getExcuse() {
+    return getWho.concat(
+      Math.floor(Math.random(getAction, getWhat, getWhen, "."))
+    );
+  }
+
+  document.getElementById("excuse").innerHTML = getExcuse();
 };
