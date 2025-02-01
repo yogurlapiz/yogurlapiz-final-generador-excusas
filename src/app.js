eb4538e1c9234e0b83376bdfe8e5b0e263b3ff2d
Vbarbosa <131600563+yogurlapiz@users.noreply.github.com>
2025-02-01 02:46:27 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -6,20 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let getWho = ["The dog", "My grandma", "The mailman", "My bird"];
-  let getAction = ["ate", "peed", "crushed", "broke"];
-  let getWhat = ["my homework", "my phone", "the car", "my book"];
-  let getWhen = [
-    "before the class",
-    "when I was sleeping",
-    "while I was exercising",
-    "during my lunch",
-    "while I was praying"
-  ];
-
   function getExcuse() {
-    return getWho.concat(
-      Math.floor(Math.random(getAction, getWhat, getWhen, "."))
+    let getWho = ["The dog", "My grandma", "The mailman", "My bird"];
+    let getAction = ["ate", "peed", "crushed", "broke"];
+    let getWhat = ["my homework", "my phone", "the car", "my book"];
+    let getWhen = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    var who = Math.floor(Math.random() * getWho.length);
+    var action = Math.floor(Math.random() * getAction.length);
+    var what = Math.floor(Math.random() * getWhat.length);
+    var when = Math.floor(Math.random() * getWhen.length);
+
+    return (
+      getWho[who] +
+      " " +
+      getAction[action] +
+      " " +
+      getWhat[what] +
+      " " +
+      getWhen[when]
     );
   }
 
